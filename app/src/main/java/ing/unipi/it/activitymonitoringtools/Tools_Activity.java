ced590b42fb6a7dabbcb91263d83d178b3103148
Carmela Rosetta <carmela.rosetta@gmail.com>
2014-10-28 15:51:14 +0100

    20th

--- app/src/main/java/ing/unipi/it/activitymonitoringtools/Tools_Activity.java
+++ app/src/main/java/ing/unipi/it/activitymonitoringtools/Tools_Activity.java
@@ -1,9 +1,7 @@
 package ing.unipi.it.activitymonitoringtools;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +14,6 @@ import android.widget.ToggleButton;
 
 import java.util.List;
 
-import ing.unipi.it.activitymonitoringtools.R;
 
 public class Tools_Activity extends Activity {
 
@@ -29,30 +26,25 @@ public class Tools_Activity extends Activity {
     private final static int POSTURE_DET = 2;
     private final static int IMPACT_DET = 3;
 
+    boolean[] active;
+
     private String smartPhonePosition;
     private List<SensorInfo> selectedSensorsList;
     private UserInformationManager userInformationManager;
-
-    boolean[] active;
-
-
     private ToggleButton enableSensorLog;
     private ToggleButton enableGaitRecog;
     private ToggleButton enablePostureDetect;
     private ToggleButton enableImpactDetect;
-
     private ImageButton settingsSensorLog;
     private ImageButton settingsGaitRecog;
     private ImageButton settingsPostureDetect;
     private ImageButton settingsImpactDetect;
-
     private Button btnStartServices;
-
 
     private void bindComponents() {
 
         active = new boolean[NUM_TOOLS];
-        for(int i=0; i < NUM_TOOLS; i++)
+        for (int i = 0; i < NUM_TOOLS; i++)
             active[i] = false;
 
         enableSensorLog = (ToggleButton) findViewById(R.id.toggleEnableSensorLog);
@@ -78,6 +70,8 @@ public class Tools_Activity extends Activity {
         enablePostureDetect.setOnCheckedChangeListener(postureDetectChangeListener);
 
         enableImpactDetect.setOnCheckedChangeListener(impactDetectChangeListener);
+
+        btnStartServices.setOnClickListener(startBtnListener);
     }
 
     CompoundButton.OnCheckedChangeListener sensorLogChangeListener = new CompoundButton.OnCheckedChangeListener() {
@@ -86,37 +80,54 @@ public class Tools_Activity extends Activity {
             active[SENSOR_LOG] = isChecked;
         }
     };
-
     CompoundButton.OnCheckedChangeListener gaitRecChangeListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             active[GAIT_REC] = isChecked;
         }
     };
-
     CompoundButton.OnCheckedChangeListener postureDetectChangeListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             active[POSTURE_DET] = isChecked;
 
-            if(isChecked && !active[GAIT_REC]) {
+            if (isChecked && !active[GAIT_REC]) {
                 enableGaitRecog.setChecked(true);
             }
         }
     };
-
     CompoundButton.OnCheckedChangeListener impactDetectChangeListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             active[IMPACT_DET] = isChecked;
         }
     };
-
     View.OnClickListener settingsSensorLogListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent i = new Intent(getApplicationContext(), SensorDataLogSettings.class);
-            startActivityForResult(i,MainActivity.SELECT_SENSORS);
+            startActivityForResult(i, MainActivity.SELECT_SENSORS);
+        }
+    };
+
+
+
+    View.OnClickListener startBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int activeTools = 0;
+            for (int k = 0; k < NUM_TOOLS; k++) {
+                if (active[k]) {
+                    activeTools++;
+                }
+            }
+
+            if (activeTools == 0) {
+                Toast.makeText(getApplicationContext(), "You didn't choose any service ", Toast.LENGTH_SHORT).show();
+            } else {
+                startServices();
+            }
+
         }
     };
 
@@ -148,7 +159,7 @@ public class Tools_Activity extends Activity {
         if (id == R.id.action_settings) {
             Intent intent = new Intent(getApplicationContext(), UserInformationActivity.class);
             startActivity(intent);
-           // finish();
+            // finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -156,13 +167,34 @@ public class Tools_Activity extends Activity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == SELECT_SENSORS) {
-            if(resultCode == SensorDataLogSettings.SENSORS_SELECTED) {
-                smartPhonePosition= data.getStringExtra("Smartphone position");
+        if (requestCode == SELECT_SENSORS) {
+            if (resultCode == SensorDataLogSettings.SENSORS_SELECTED) {
+                smartPhonePosition = data.getStringExtra("Smartphone position");
                 //Toast.makeText(getApplicationContext(), smartPhonePosition, Toast.LENGTH_SHORT).show();
                 selectedSensorsList = (List<SensorInfo>) data.getSerializableExtra("Selected sensors");
             }
 
         }
+    }
+
+    public void startServices() {
+        if(enableSensorLog.isChecked()) {
+            Toast.makeText(getApplicationContext(), "SENSOR LOG ", Toast.LENGTH_SHORT).show();
+        }
+
+        if(enableGaitRecog.isChecked()) {
+            Toast.makeText(getApplicationContext(), "GAIT ", Toast.LENGTH_SHORT).show();
+        }
+
+        if(enablePostureDetect.isChecked()) {
+            Toast.makeText(getApplicationContext(), "POSTURE ", Toast.LENGTH_SHORT).show();
+        }
+
+        if(enableImpactDetect.isChecked()) {
+            Toast.makeText(getApplicationContext(), "IMPACT ", Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 }
